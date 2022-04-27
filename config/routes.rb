6dc6fc3fade1a6ed1df8dd944d2dadbdf982b05a
Donovan Young <dyoung522@gmail.com>
2022-04-27 00:38:37 -0400

    Add Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello/world'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "hello/world"
+  root "home#index"
 end
