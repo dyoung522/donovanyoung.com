0587169ffd96ac1ac93c624e532c222649730568
Donovan Young <dyoung522@gmail.com>
2022-05-01 21:05:17 -0400

    Add favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_page_title
+
+  def set_page_title
+    @page_title = "Donovan's Web"
+    @page_heading = "Home"
+  end
 end
